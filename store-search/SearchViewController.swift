7a2d1ef0370756756d61dec88a19696bce015392
Erika Languirand <erika@detroitlabs.com>
2018-09-30 16:01:51 -0400

    Setting up outlets

--- store-search/SearchViewController.swift
+++ store-search/SearchViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Propertiesr
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
