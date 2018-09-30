a3781d2f2aba761aa54e580daa058afcef68bc2d
Erika Languirand <erika@detroitlabs.com>
2018-09-30 15:47:10 -0400

    Initial commit

---
+++ store-search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  store-search
+//
+//  Created by Erika Carlson on 9/30/18.
+//  Copyright © 2018 Erika Carlson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
